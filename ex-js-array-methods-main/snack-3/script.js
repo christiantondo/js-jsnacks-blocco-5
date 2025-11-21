33d99e899dd5e957c65240f7ddb7f38d867242c0
christiantondo <christiantondo1@gmail.com>
2025-11-21 14:59:32 +0100

    snack3 completed

--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -1,4 +1,6 @@
 const numbers = [2, 8, 4, 7, 2, 87];
-
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const numbersPlus1 = numbers.map((number) => number + 1)
+console.log("Stampo il nuovo array, numeri + 1", numbersPlus1);
